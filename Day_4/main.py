3810746753f4457454a49eb0f7f89feb6c8b4118
Arnab Mitra <arnabmitra471@gmail.com>
2024-11-17 22:27:18 +0530

    Day 4 completed

--- Day_4/main.py
+++ Day_4/main.py
@@ -1,1 +1,9 @@
 # This is the main.py file for day 4
+
+print('I am Arnab Mitra','I love python a lot')
+print("Hello World","I am 20 years old")
+print(10)
+print("Bye")
+
+print(17 * 13)
+print(30*34)
