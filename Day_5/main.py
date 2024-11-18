8aea101bd85cc19b096178c622e59b81388f98cf
Arnab Mitra <arnabmitra471@gmail.com>
2024-11-18 21:50:57 +0530

    Day 5 completed

--- Day_5/main.py
+++ Day_5/main.py
@@ -1,1 +1,35 @@
 # This is the main.py file for day 5
+'''
+Hey Arnab, please don't remove this line
+Author: Arnab
+'''
+# New line escape sequence
+# print("I am Arnab Mitra")
+print("Hey !! I am a good boy\n and this viewer is also a good boy/girl") # This is a single line comment
+print(34/4)
+
+# This is a single line comment
+# print("Hello World")
+# print("Hi everyone !!!") # Printing Hello World
+
+# \' and \"
+print("We are the so called \"Vikings\" from the north")
+print('We are the so called \'Vikings\' from the north')
+
+# Mixed Quotes
+print("We are the so called 'Vikings' from the north")
+print('We are the so called "Vikings" from the north')
+
+# Horizontal tab escape sequence
+print("My name is Arnab \t and my friend's name is Shubham")
+print("My name is Arnab and  my friend's name is\r Shubham")
+
+print("The lion is roaring \v\v a lot. Seems like \v\v he is hungry !!")
+print("Hey this is a lion")
+
+# More on print statement
+
+# Multiple values passed to the print function are printed in sequence,separated by a space between them.
+print("Hey There !!",24,87,sep="~")
+print("Hi I am Arnab","I am 20 years old","I love programming a lot","Python is very elegant !!",sep="~",end="\t")
+print("This is Shaarav")
