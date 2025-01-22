672f04f5de0f185488a4edfa30814001347a25fe
Arnab Mitra <arnabmitra471@gmail.com>
2025-01-22 22:32:20 +0530

    Exercise for day 8 completed arithmetic operators in python

--- Day_8/main.py
+++ Day_8/main.py
@@ -1,1 +1,18 @@
-# This is the main.py file for day 8
+# This is the main.py file for day 7
+
+num1 = 35
+num2 = 12
+total = num1 + num2
+diff = num1 - num2
+prod = num1 * num2
+quot = num1 / num2
+floor_div = num1 // num2
+rem = num1 % num2
+exp = num1 ** num2
+print("The sum of",num1,"and",num2,"is",total)
+print("The difference of",num1,"and",num2,"is",diff)
+print("The product of",num1,"and",num2,"is",prod)
+print("The quotient after dividing",num1,"and",num2,"is",quot)
+print("The floor division quotient of",num1,"and",num2,"is",floor_div)
+print("The remainder obtained after division of",num1,"and",num2,"is",rem)
+print(num1,"raised to the power of",num2,"is",exp)
