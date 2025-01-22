d5dbad0f82f5330a665359e1a1e8e43b34a08411
Arnab Mitra <arnabmitra471@gmail.com>
2025-01-22 10:06:24 +0530

    Day 7 completed

--- Day_7/main.py
+++ Day_7/main.py
@@ -1,1 +1,18 @@
 # This is the main.py file for day 7
+
+# arithmetic operators
+print(15 + 6)
+print(15 - 6)
+print(15 * 6)
+print(15 / 6)
+print(15//6)
+print(15 % 6)
+print(5 ** 3)
+print(5 ** 5)
+print(2 ** 4)
+print(15/3)
+print(15//3)
+print(15//3)
+print(5//3)
+print(15%6)
+print(78.6%2)
