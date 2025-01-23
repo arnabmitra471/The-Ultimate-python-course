df6c9e4cab2dc52f14f25a8c3476a4be65eb373a
Arnab Mitra <arnabmitra471@gmail.com>
2025-01-23 11:26:13 +0530

    Day 9 completed typecasting

--- Day_12/main.py
+++ Day_12/main.py
@@ -1,1 +1,22 @@
 # This is the main.py file for day 12
+
+name = "I am Arnab Mitra"
+print(name)
+
+names = "Arnab,Vrinda,Shaarav,Khushi,Nishita"
+print(names)
+
+# print(names[0])
+# print(names[1])
+# print(names[2])
+
+print(names[0:5])
+print(names[0:4])
+print(names[:5])
+
+# Looping through a string
+for name in names:
+    print(name)
+
+print(len(names))
+
--- Day_9/main.py
+++ Day_9/main.py
@@ -1,1 +1,25 @@
 # This is the main.py file for day 9
+
+# Explicit typecasting
+num1 = "25"
+num2 = "36"
+print(type(num1))
+print(type(num2))
+print(int(num1) + int(num2))
+
+string = "15"
+number = 7
+string_num = int(string)
+total = number + string_num
+
+print("The sum of both the numbers is",total)
+
+# Implicit typecasting
+c = 1.9
+print(type(c))
+d = 8
+print(type(d))
+res = c + d
+
+print(res)
+print(type(res))
